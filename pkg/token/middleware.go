535d895d50cf5270498962c28dd1fc8e612a3749
f3rix-xyz <ayush_g@ar.iitr.ac.in>
2025-02-24 20:12:39 +0530

    in middleware file changed it from text-plain to content-json

--- pkg/token/middleware.go
+++ pkg/token/middleware.go
@@ -2,15 +2,27 @@ package token
 
 import (
     "context"
+    "encoding/json"
     "net/http"
     "strings"
 
     "github.com/golang-jwt/jwt/v5"
 )
 
+type ErrorResponse struct {
+    Success bool   `json:"success"`
+    Message string `json:"message"`
+}
+
 var (
-    errInvalidHeader = []byte("Invalid Authorization header format")
-    errInvalidToken  = []byte("Invalid token")
+    errInvalidHeader = ErrorResponse{
+        Success: false,
+        Message: "Invalid Authorization header format",
+    }
+    errInvalidToken = ErrorResponse{
+        Success: false,
+        Message: "Invalid token",
+    }
 )
 
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -21,12 +33,13 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
     secret := getSecret()
 
     return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        
         authHeader := r.Header.Get("Authorization")
         prefix, tokenString, ok := strings.Cut(authHeader, " ")
         if !ok || !strings.EqualFold(prefix, "Bearer") {
-            w.Header().Set("Content-Type", "text/plain")
             w.WriteHeader(http.StatusUnauthorized)
-            w.Write(errInvalidHeader)
+            json.NewEncoder(w).Encode(errInvalidHeader)
             return
         }
 
@@ -36,16 +49,14 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
         })
 
         if err != nil || !token.Valid {
-            w.Header().Set("Content-Type", "text/plain")
             w.WriteHeader(http.StatusUnauthorized)
-            w.Write(errInvalidToken)
+            json.NewEncoder(w).Encode(errInvalidToken)
             return
         }
 
         if claims.UserID == 0 {
-            w.Header().Set("Content-Type", "text/plain")
             w.WriteHeader(http.StatusUnauthorized)
-            w.Write(errInvalidToken)
+            json.NewEncoder(w).Encode(errInvalidToken)
             return
         }
 
